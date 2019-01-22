60a5098e960f6302358082a8f17e705747e942a1
marie goudon <marie.goudon@laposte.net>
2019-01-23 00:06:30 +0100

    ajout dun menu---pas de lien avec les fichiers du github cree

--- app.rb
+++ app.rb
@@ -6,21 +6,9 @@ require 'scrapper'
 
 Scrapper.new.perform
 
-require "google_drive"
-session = GoogleDrive::Session.from_config("config.json")
-#ouvrir un spreadsheet à partir de ruby 
-ws=session.spreadsheet_by_key("pz7XtlQC-PYx-jrVMJErTcg").worksheets[0]
-p ws[2, 1]
-ws[2, 1] = "foo"
-ws[2, 2] = "bar"
-ws.save
-
-(1..ws.num_rows).each do |row|
-	(1..ws.num_cols).each do |col|
-		p ws[row, col]
-	end
+cli = HighLine.new
+answer = cli.choose do |menu|
+  menu.prompt = "Please choose your favorite programming language?  "
+  menu.choice(:ruby) { say("Good choice!") }
+  menu.choices(:python, :perl) { say("Not from around here, are you?") }
 end
-
-p ws.rows
-
-ws.reload
