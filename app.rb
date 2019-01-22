eb03b8d1b07c676d508866661f4badd4064d69cb
marie goudon <marie.goudon@laposte.net>
2019-01-23 00:32:18 +0100

    ajout de lurl en entree de la fonction

--- app.rb
+++ app.rb
@@ -9,7 +9,7 @@ Scrapper.new.perform
 cli = HighLine.new
 answer = cli.choose do |menu|
   menu.prompt = "Please choose your favorite format for output?  "
-  menu.choice(:json) { save_as_JSON(fichier_json) }
-  menu.choice(:spreadsheet) { save_as_spreadsheet(spreadsheet) }
+  menu.choice(:json) { save_as_JSON("http://www.annuaire-des-mairies.com/val-d-oise.html") }
+  menu.choice(:spreadsheet) { save_as_spreadsheet("email.JSON") }
   menu.choice(:csv) {save_as_csv(fichier_csv) }
 end
