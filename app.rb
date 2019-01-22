00612ecb5eb0fd0650953d10879b76d8c90530db
marie goudon <marie.goudon@laposte.net>
2019-01-23 00:27:38 +0100

    modification du parametre de la methode save as spreadsheet

--- app.rb
+++ app.rb
@@ -10,6 +10,6 @@ cli = HighLine.new
 answer = cli.choose do |menu|
   menu.prompt = "Please choose your favorite format for output?  "
   menu.choice(:json) { save_as_JSON(fichier_json) }
-  menu.choice(:spreadsheet) { save_as_spreadsheet(sheet) }
+  menu.choice(:spreadsheet) { save_as_spreadsheet(spreadsheet) }
   menu.choice(:csv) {save_as_csv(fichier_csv) }
 end
