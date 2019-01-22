1359577eb098bf57b453002fbbb3e84ba3236aaa
marie goudon <marie.goudon@laposte.net>
2019-01-22 15:48:18 +0100

    ajout de tests

---
+++ spec/scrapper_spec.rb
@@ -0,0 +1,15 @@
+require_relative "../../spec/scrapper_spec.rb"
+describe "the require line '../../db/emails.jsoné" do
+it "should load the file situated in the specified path" do *
+expect("require '../../db/emails.json'").to eq(File.open() 
+end
+end
+describe "the Scrapper class" do 
+it "should scrap the city council's web pages to get the email addresses and the names of the cities" do 
+end
+describe "the save as JSON method" do 
+it "should write inside of a email.JSON file" do 
+end
+descrine "the save as spreadsheet method" do 
+it "should write inside of a Google spreadsheet" do
+end
