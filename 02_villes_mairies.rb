45f07df2b7ad0fcb163d14308d9533d247dd2f99
marie goudon <marie.goudon@laposte.net>
2019-01-22 11:32:18 +0100

    rajout d'une methode save as spreadsheet et dune methode save as csv

--- 02_villes_mairies.rb
+++ 02_villes_mairies.rb
@@ -1,4 +1,5 @@
 require 'rubygems'
+require "google_drive"
 require 'json'
 require 'nokogiri'
 require 'open-uri'
@@ -38,14 +39,55 @@ tempHash=Hash[villes.zip(emails)]
 File.open("email.JSON","w") do |f|
 	f.write(tempHash.to_json)
 end
+f.close
 		#Hash[villes.zip(emails)].each do |ville,email|
 			#f.puts(ville+" => "+email)
 #my_hash=Hash[villes.zip(emails)]
 #return my_hash(ville_entree)
 		end
 	def save_as_spreadsheet
-		
-	end
-	end
-	
+		session = GoogleDrive::Session.from_vonfig("config.json")
 
+ws = session.spreadsheet_by_key("pz7Xt1QC-PYx-jrVMJErTcg").worksheets[0]
+
+p ws[2, 1]
+g = File.open('email.JSON','w')
+col1=1
+lin1=2
+col2=2
+lin2=2
+File.foreach( 'email.JSON' ) do |line|
+	ligne=line.split("=>")
+	ws[lin1,col1] = ligne[0]
+	ws[lin2,col2] = ligne[1]
+	ws.save
+	lin1+=1
+	lin2+=1
+end	
+		
+end
+#ws[2, 1] = "foo"
+#ws[2, 2] = "bar"
+#ws.save
+
+(1..ws.num_rows).each do |row|
+	(1..ws._num_cols).each do |col|
+		p ws[row, col]
+	end
+def save_as_csv
+	h = File.open("email.csv","a")
+File.foreach ('email.JSON') do |line|
+	ligne=line.split(" ")
+	ligne2=ligne.delete("=>")
+	h.puts(ligne2[0],',',ligne2[1..ligne2.length])
+h.close
+
+end
+	
+end
+end
+
+p ws.rows
+
+ws.reload
+	end
