992a955f904438bf3d088a5a4927ba9b6e7eb4d7
marie goudon <marie.goudon@laposte.net>
2019-01-22 16:33:48 +0100

    modifications des tests

--- spec/scrapper_spec.rb
+++ spec/scrapper_spec.rb
@@ -12,9 +12,11 @@ it "should write inside of a email.JSON file" do
 end
 describe "the save as spreadsheet method" do 
 it "should write inside of a Google spreadsheet" do
+expect(save_as_spreadsheet("email.JSON",i)).to eq(["@"].include?())
+end
 end
 describe "the save as csv method method" do 
 it "should write inside of a csv file" do 
-expect(save_as_csv("email.JSON",i)).to eq([","].include?(i))
+expect(save_as_csv("email.JSON")).to eq([","].include?())
 end
-
+end
