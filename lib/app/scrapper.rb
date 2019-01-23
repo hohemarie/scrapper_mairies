bbeb466d57653b0e849776c2741cbd5fe2de429f
marie goudon <marie.goudon@laposte.net>
2019-01-23 08:44:20 +0100

    indentation

--- lib/app/scrapper.rb
+++ lib/app/scrapper.rb
@@ -40,59 +40,47 @@ class Scrapper
 			end
 	
 		end
-tempHash=Hash[villes.zip(emails)]
-File.open("email.JSON","w") do |fichier_json|
-	fichier_json.write(tempHash.to_json)
-end
-fichier_json.close
+		tempHash=Hash[villes.zip(emails)]
+		File.open("email.JSON","w") do |fichier_json|
+			fichier_json.write(tempHash.to_json)
+		end
+		fichier_json.close
 		#Hash[villes.zip(emails)].each do |ville,email|
 			#f.puts(ville+" => "+email)
-#my_hash=Hash[villes.zip(emails)]
-#return my_hash(ville_entree)
-		end
+		#my_hash=Hash[villes.zip(emails)]
+		#return my_hash(ville_entree)
+	end
 	def save_as_spreadsheet(fichier_json)
 		session = GoogleDrive::Session.from_config("config.json")
 
-ws = session.spreadsheet_by_key("pz7Xt1QC-PYx-jrVMJErTcg").worksheets[0]
+		ws = session.spreadsheet_by_key("pz7Xt1QC-PYx-jrVMJErTcg").worksheets[0]
 
-p ws[2, 1]
-g = File.open(fichier_json,'w')
-col1=1
-lin1=2
-col2=2
-lin2=2
-File.foreach( fichier_json ) do |line|
-	ligne=line.split("=>")
-	ws[lin1,col1] = ligne[0]
-	ws[lin2,col2] = ligne[1]
-	ws.save
-	lin1+=1
-	lin2+=1
-end	
+		p ws[2, 1]
+		g = File.open(fichier_json,'w')
+		col1=1
+		lin1=2
+		col2=2
+		lin2=2
+		File.foreach( fichier_json ) do |line|
+			ligne=line.split("=>")
+			ws[lin1,col1] = ligne[0]
+			ws[lin2,col2] = ligne[1]
+			ws.save
+			lin1+=1
+			lin2+=1
+		end	
 		
-end
-#ws[2, 1] = "foo"
-#ws[2, 2] = "bar"
-#ws.save
-
-(1..ws.num_rows).each do |row|
-	(1..ws._num_cols).each do |col|
-		p ws[row, col]
 	end
-def save_as_csv(fichier_json)
-	h = File.open("email.csv","a")
-File.foreach (fichier_json) do |line|
-	ligne=line.split(" ")
-	ligne2=ligne.delete("=>")
-	h.puts(ligne2[0]+','+ligne2[1..ligne2.length]+'\n')
-h.close
+	def save_as_csv(fichier_json)
+		h = File.open("email.csv","a")
+		File.foreach (fichier_json) do |line|
+			ligne=line.split(" ")
+			ligne2=ligne.delete("=>")
+			h.puts(ligne2[0]+','+ligne2[1..ligne2.length]+'\n')
+		h.close
 
-end
+		end
 	
-end
+	end
 end
 
-p ws.rows
-
-ws.reload
-end
